2434dbc2230148ac2f5a1ff1643c6e9341c94563
Joao paulo chaves <joaopaulochaves@MacBook-Pro-de-Joao.local>
2022-02-13 17:25:52 -0300

    Inicial

---
+++ src/components/home/menu/TopMenu.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import '../../../App.css';
+import '../../../content/css/main.css';
+import '../../../content/css/vendor.css';
+import '../../../content/css/base.css';
+
+
+function TopMenu() {
+    return (
+        <header id="header" className="row">
+            <div className="header-logo">
+                <a href="index">neo. Coffee</a>
+            </div>
+            <nav id="header-nav-wrap">
+                <ul className="header-main-nav">
+                    <li className="current"><a className="smoothscroll" href="#home" >Bem Vindo</a></li>
+                    <li><a className="smoothscroll" href="#about" title="Sobre" ></a>Neo</li>
+                    <li><a className="smoothscroll" href="#yours" title="Seu Café"></a>Seu Café</li>
+                    <li><a className="smoothscroll" href="#sigin" title="Cadastre"></a>Cadastre</li>
+                </ul>
+
+            </nav>
+
+        </header>
+    );
+
+}
+
+export default TopMenu;
